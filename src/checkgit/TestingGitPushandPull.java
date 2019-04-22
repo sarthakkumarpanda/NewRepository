3ff86e3ccb8c6f4d108876c764d932f57d072692
sarthakkumarpanda <sarthak.matrix@gmail.com>
2019-04-22 14:13:25 +0530

    Firstcommit

---
+++ src/checkgit/TestingGitPushandPull.java
@@ -0,0 +1,43 @@
+package checkgit;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+public class TestingGitPushandPull {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\SELENIUM\\BROWSER DRIVERS\\CHROMEDRIVER\\chromedriver.exe");
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "D://chrome.log");
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+		System.out.println(allLinks.size());
+		
+		for(int i=1; i<=allLinks.size(); i++){
+			System.out.println(allLinks.get(i).getText());
+		}
+				
+				
+		driver.quit();		
+				
+				
+		
+		
+
+	}
+
+}
